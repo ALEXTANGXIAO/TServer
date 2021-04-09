bb46287ce25dd201608467e738978b47720eb4ef
574809918@qq.com <574809918@qq.com>
2021-04-09 20:53:47 +0800

    Log Update

--- SocketServer/Tool/Debug.cs
+++ SocketServer/Tool/Debug.cs
@@ -16,8 +16,8 @@ namespace SocketServer
     {
         static public void Log(string str)
         {
-            string Message = String.Format("{0}|{1}|", LogLevel.DEBUG,DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
-
+            Console.ForegroundColor = ConsoleColor.White;
+            string Message = String.Format("{0} |{1}|", LogLevel.DEBUG,DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
             Console.WriteLine(Message);
         }
 
@@ -31,14 +31,14 @@ namespace SocketServer
         static public void LogError(string str)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            string Message = String.Format("{0}|{1}|", LogLevel.ERROR, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
+            string Message = String.Format("{0} |{1}|", LogLevel.ERROR, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
             Console.WriteLine(Message);
         }
 
         static public void LogInfo(string str)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            string Message = String.Format("{0}|{1}|", LogLevel.INFO, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
+            string Message = String.Format("{0}  |{1}|", LogLevel.INFO, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")) + str;
             Console.WriteLine(Message);
         }
 
@@ -63,13 +63,13 @@ namespace SocketServer
             Console.WriteLine(Message);
         }
 
-        private static string s = "\r\n   ___ _                 _\r\n  / __\\ | ___  _   _  __| |_ __ _____   _____\r\n / /  | |/ _ \\| | | |/ _ | '__/ _ \\ \\ / / _ \\\r\n/ /___| | (_) | |_| | (_| | | |  __/\\ V /  __/\r\n\\____/|_|\\___/ \\__,_|\\__,_|_|  \\___| \\_/ \\___|\r\n\r\n   V1.0.0  Commit #TANG  Pro=true\r\n================================================\r\n\r\n[Info]    {0} 初始化数据库连接\r\n[Info]    {1} 数据库版本匹配，跳过数据库迁移\r\n[Info]    {2} 初始化任务队列，WorkerNum = 10\r\n[Info]    {3} 初始化定时任务...\r\n[Info]    {4} 当前运行模式：Master";
+        private static string s = "\r\n   ___ _                 _\r\n  / __\\ | ___  _   _  __| |_ __ _____   _____\r\n / /  | |/ _ \\| | | |/ _ | '__/ _ \\ \\ / / _ \\\r\n/ /___| | (_) | |_| | (_| | | |  __/\\ V /  __/\r\n\\____/|_|\\___/ \\__,_|\\__,_|_|  \\___| \\_/ \\___|\r\n\r\n   V1.0.0  Commit #TANG  Pro=true\r\n================================================\r\n\r\n[Info]    {0} 初始化数据库连接\r\n[Info]    {1} 数据库版本匹配，跳过数据库迁移\r\n[Info]    {2} 初始化任务队列，WorkerNum = 10\r\n[Info]    {3} 初始化定时心跳包...\r\n[Info]    {4} 初始化帧同步：True\r\n[Info]    {5} 当前运行模式：Master";
 
         static public void LogApp()
         {
             string Message = String.Format(s, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"),
                 DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"), DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"),
-                DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"), DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+                DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"), DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"), DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
             Debug.Log(Message);
         }
     }
